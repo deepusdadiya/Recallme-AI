9e66b0f89b10d7ce5af72994a8ece84baca8d758
deepusdadiya <deepusdadiyaofficial@gmail.com>
2025-05-21 08:55:19 +0530

    databse session

--- backend/alchemist/postgresql/resource.py
+++ backend/alchemist/postgresql/resource.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
+from .functions import Memory
+import os
 
-DATABASE_URL = "sqlite:///./test.db"  # or your actual DB path
+DATABASE_URL = "jdbc:postgresql://192.168.62.114:5432/postgres"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -11,3 +13,10 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def save_memory(db: Session, memory_data: dict):
+    db_memory = Memory(**memory_data)
+    db.add(db_memory)
+    db.commit()
+    db.refresh(db_memory)
+    return db_memory
