71fa81d7430107d173c901f4d6c3d83ec8e8cf2e
Deep Usdadiya <deepusdadiyaofficial@gmail.com>
2025-05-23 18:13:29 +0530

    summary store in DB

--- backend/services/memory_pipeline/service.py
+++ backend/services/memory_pipeline/service.py
@@ -2,14 +2,16 @@ from config.config import DUMMY_USER_ID
 from vectorstore.faiss_service import store_text
 from alchemist.postgresql.initializer import SourceType
 from alchemist.postgresql.resource import save_memory
+from services.memory_pipeline.summarizer import summarize_text
 
 def process_memory(db, title: str, source_type: str, raw_text: str):
+    summary = summarize_text(raw_text)
     memory_data = {
         "user_id": DUMMY_USER_ID,
         "title": title,
         "source_type": SourceType(source_type),
         "raw_text": raw_text,
-        "summary": None
+        "summary": summary
     }
     memory = save_memory(db, memory_data)
 
